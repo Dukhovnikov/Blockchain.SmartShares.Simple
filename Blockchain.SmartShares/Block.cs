641b2a7292c08bb7980bb89e069be70c01b8e02e
Danil <DukhovnikovDanil@yandex.ru>
2017-10-12 23:12:17 +0300

    Work for blockchain was started. Added class block.

---
+++ Blockchain.SmartShares/Block.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Blockchain.SmartShares
+{
+    public class Block
+    {
+        public int Id { get; private set; }
+        public DateTime Timestamp { get; set; }
+        public string Hash { get; private set; }
+        public string PreviuosHash { get; private set; }
+    }
+    
+     
+}
