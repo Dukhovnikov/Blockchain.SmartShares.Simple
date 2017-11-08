30a8d58ab17f7aaf45e3e9131d2175a4c759818a
Данил <duhovnikovd@gmail.com>
2017-11-09 02:42:47 +0300

    Some added

---
+++ Blockchain.SmartShares/Network/Connection.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace Blockchain.SmartShares.Network
+{
+    public class Connection
+    {
+        public int Port { get; set; }
+        
+        public IPAddress Address { get; set; }
+        
+        
+    }
+}
