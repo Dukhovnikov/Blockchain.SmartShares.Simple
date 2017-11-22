55afcba7abceadf5714261352f099978ebf59bd0
Danil <DukhovnikovDanil@yandex.ru>
2017-11-22 12:41:11 +0300

    Создан проект, дублирующий предыдущий, в качестве главного проекта

---
+++ SmartShares/Models.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using MessagePack;
+using Newtonsoft.Json;
+
+namespace SmartShares
+{
+    [MessagePackObject]
+    public class Block
+    {
+        [Key(0), JsonProperty("id")]
+        public int Id { get; set; }
+        
+        [Key(1), JsonProperty("hash")]
+        public byte[] Hash { get; set; }
+        
+        [Key(2), JsonProperty("prevhash")]
+        public byte[] PreviousHash { get; set; }
+        
+        [Key(3), JsonProperty("timestamp")]
+        public DateTime Timestamp { get; set; }
+        
+        [Key(4), JsonProperty("transaction")]
+        public Transaction Transaction { get; set; }
+
+        public Block Clone() =>
+            new Block()
+            {
+                Id = Id,
+                Hash = Hash,
+                PreviousHash = PreviousHash,
+                Timestamp = Timestamp,
+                Transaction = Transaction
+            };
+    }
+    
+    [MessagePackObject]
+    public sealed class Transaction
+    {
+        [Key(0), JsonProperty("id")]
+        public ulong Id { get; set; }
+        
+        [Key(1), JsonProperty("sign")]
+        public byte[] Signature { get; set; }
+        
+        [Key(2), JsonProperty("timestamp")]
+        public DateTime Timestamp { get; set; }
+        
+        [Key(3), JsonProperty("in")]
+        public IList<InEntry> InEntries { get; set; }
+        
+        [Key(4), JsonProperty("out")]
+        public IList<OutEntry> OutEntries { get; set; }
+    }
+    
+    [MessagePackObject]
+    public class InEntry
+    {
+        [Key(0), JsonProperty("prevOut")]
+        public byte[] PreviuosOut { get; set; }
+        
+        [Key(1), JsonProperty("publicKey")]
+        public byte[] PublicKey { get; set; }
+    }
+
+    [MessagePackObject]
+    public class OutEntry
+    {
+        [Key(0), JsonProperty("to")]
+        public byte[] RecipientHash { get; set; }
+        
+        [Key(1), JsonProperty("val")]
+        public ulong Value { get; set; }
+    }
+}
---
+++ SmartShares/Util/EccService.cs
@@ -0,0 +1,92 @@
+﻿using System.Security.Cryptography;
+using MessagePack;
+
+namespace SmartShares.Util
+{
+    public static class EccService
+    {
+        /// <summary>
+        /// Получает именованную кривую nistP256.
+        /// </summary>
+        private static readonly ECCurve Curve = ECCurve.NamedCurves.nistP256;
+
+        public static void GenerateKey(out byte[] privateKey, out byte[] publicKey)
+        {
+            //Представляет стандартные параметры для алгоритма шифрования на основе эллиптических кривых (ECC).
+            ECParameters param;
+            
+            //Создает новый экземпляр заданной реализации из эллиптических кривых цифровой подписи алгоритма (ECDSA).
+            using (var dsa = ECDsa.Create(Curve))
+                param = dsa.ExportParameters(true);
+            
+            privateKey = param.D;
+            publicKey = ToBytes(param.Q);
+        }
+
+        public static byte[] Sign(byte[] hash, byte[] privateKey, byte[] publicKey)
+        {
+            var param = new ECParameters()
+            {
+                D = privateKey,
+                Q = ToEcPoint(publicKey),
+                Curve = Curve
+            };
+
+            using (var dsa = ECDsa.Create(param))
+                return dsa.SignHash(hash);
+        }
+
+        public static bool Verify(byte[] hash, byte[] signature, byte[] publicKey)
+        {
+            var param = new ECParameters()
+            {
+                Q = ToEcPoint(publicKey),
+                Curve = Curve
+            };
+
+            using (var dsa = ECDsa.Create(param))
+                return dsa.VerifyHash(hash, signature);
+        }
+
+        public static bool TestKey(byte[] privateKey, byte[] publicKey)
+        {
+            byte[] testHash;
+
+            using (var sha = SHA256.Create())
+                testHash = sha.ComputeHash(new byte[0]);
+
+            try
+            {
+                var signature = Sign(testHash, privateKey, publicKey);
+                return Verify(testHash, signature, publicKey);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static byte[] ToBytes(ECPoint point)
+        {
+            return MessagePackSerializer.Serialize(
+                new FormattableEcPoint {X = point.X, Y = point.Y});
+        }        
+
+        private static ECPoint ToEcPoint(byte[] bytes)
+        {
+            var ecPoint = MessagePackSerializer.Deserialize<FormattableEcPoint>(bytes);
+            return new ECPoint {X = ecPoint.X, Y = ecPoint.Y};
+        }      
+        
+        [MessagePackObject]
+        // ReSharper disable once MemberCanBePrivate.Global
+        public sealed class FormattableEcPoint
+        {
+            [Key(0)]
+            public byte[] X { get; set; }
+            
+            [Key(1)]
+            public byte[] Y { get; set; }
+        }
+    }
+}
